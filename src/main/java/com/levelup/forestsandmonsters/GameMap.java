7cfe5cbf337cb386ee308467f0c7b461c7692d5e
midnortexcoder1 <stan@momurrays.com>
2023-11-16 14:37:10 -0500

    Game Map total positions

--- src/main/java/com/levelup/forestsandmonsters/GameMap.java
+++ src/main/java/com/levelup/forestsandmonsters/GameMap.java
@@ -1,5 +1,15 @@
 package com.levelup.forestsandmonsters;
 
 public class GameMap {
+
+    int totalPositions=0;
+
+    public int getTotalPositions() {
+        return totalPositions;
+    }
     
+    public void setTotalPositions(int pos) {
+        totalPositions=pos;
+    }
+  
 }
