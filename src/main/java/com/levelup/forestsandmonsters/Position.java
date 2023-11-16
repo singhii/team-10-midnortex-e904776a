1203e0d49d469e7f1c677a422a9adf4a62e8193b
Isha <singhi@mail.gvsu.edu>
2023-11-16 12:31:11 -0500

    Position class shell

---
+++ src/main/java/com/levelup/forestsandmonsters/Position.java
@@ -0,0 +1,13 @@
+package com.levelup.forestsandmonsters;
+import java.awt.Point;
+
+public class Position {
+
+    Point coordinates;
+
+    public  Position(int xCoordinates, int yCoordinates)
+    {
+      
+    }
+    
+}
