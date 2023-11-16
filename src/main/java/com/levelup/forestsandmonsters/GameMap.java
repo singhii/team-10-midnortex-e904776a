794089b7ec9d6a51da0bd1c859787bf51d7d53ab
Isha <singhi@mail.gvsu.edu>
2023-11-16 18:08:40 -0500

    Game Map changes

--- src/main/java/com/levelup/forestsandmonsters/GameMap.java
+++ src/main/java/com/levelup/forestsandmonsters/GameMap.java
@@ -2,6 +2,10 @@ package com.levelup.forestsandmonsters;
 
 import java.awt.Point;
 
+import javax.naming.directory.DirContext;
+
+import org.stringtemplate.v4.compiler.STParser.element_return;
+
 import com.levelup.forestsandmonsters.GameController.DIRECTION;
 
 public class GameMap {
@@ -16,11 +20,45 @@ public class GameMap {
         totalPositions=pos;
     }
 
-    public Point calculatePosition(Position startingPosition, DIRECTION direction)
+    public Position calculatePosition(Position startingPosition, DIRECTION direction)
     {
-        // starting position
-        // direction
-        Point calPosition = new Point(0, 0);
+       
+        Position calPosition = new Position(0,0);
+
+        int startPositionX = startingPosition.getPosition().x;
+        int startPositionY = startingPosition.getPosition().y;
+        int newPositionX = 0;
+        int newPositionY = 0;
+
+        
+        if (direction.equals(DIRECTION.EAST))
+        {
+           //startPositionX = startingPosition.getPosition().x;
+           newPositionX = startPositionX++;
+          
+        }
+        else if(direction.equals(DIRECTION.WEST))
+        {
+            //startPositionX = startingPosition.getPosition().x;
+            newPositionX = startPositionX--;
+        }
+        else if(direction.equals(DIRECTION.NORTH))
+        {
+            //startPositionY = startingPosition.getPosition().y;
+            newPositionY = startPositionY++;
+        }
+        else if(direction.equals(DIRECTION.SOUTH))
+        {
+            //startPositionY = startingPosition.getPosition().y;
+            newPositionY = startPositionY--;
+        }
+        else{
+            newPositionX = startPositionX;
+            newPositionY = startPositionY;
+        }
+
+        calPosition.setPosition(newPositionX, newPositionY);
+
         return calPosition;
     }
   
--- src/test/java/com/levelup/forestsandmonsters/GameMapTest.java
+++ src/test/java/com/levelup/forestsandmonsters/GameMapTest.java
@@ -1,8 +1,13 @@
 package com.levelup.forestsandmonsters;
 
 import org.junit.Test;
+import com.levelup.forestsandmonsters.Position;
+import com.levelup.forestsandmonsters.GameController.DIRECTION;
+
+import ch.qos.logback.core.pattern.PostCompileProcessor;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.startsWith;
 
 public class GameMapTest {
 
@@ -22,5 +27,20 @@ public class GameMapTest {
         int actualMoves=g.getTotalPositions();
         
         assertEquals(expectedMoves,actualMoves);
+    }
+
+    @Test
+    public void testCalculatePosition()
+    {
+        GameMap gameMap = new GameMap();
+        
+        Position startingPosition = new Position(0, 0);
+        DIRECTION direction = DIRECTION.EAST;
+
+       Position newPosition = gameMap.calculatePosition(startingPosition, direction);
+    
+       assertEquals(0, newPosition.getPosition().x); 
+       assertEquals(0, newPosition.getPosition().y); 
+
     }
 }
