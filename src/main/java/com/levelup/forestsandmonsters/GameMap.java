53580be0846f4e7fbd1ce688719e3990c097d134
Isha <singhi@mail.gvsu.edu>
2023-11-16 15:52:13 -0500

    incomplete calculate position method

--- src/main/java/com/levelup/forestsandmonsters/GameMap.java
+++ src/main/java/com/levelup/forestsandmonsters/GameMap.java
@@ -1,5 +1,9 @@
 package com.levelup.forestsandmonsters;
 
+import java.awt.Point;
+
+import com.levelup.forestsandmonsters.GameController.DIRECTION;
+
 public class GameMap {
 
     int totalPositions=0;
@@ -10,6 +14,14 @@ public class GameMap {
     
     public void setTotalPositions(int pos) {
         totalPositions=pos;
+    }
+
+    public Point calculatePosition(Position startingPosition, DIRECTION direction)
+    {
+        // starting position
+        // direction
+        Point calPosition = new Point(0, 0);
+        return calPosition;
     }
   
 }
