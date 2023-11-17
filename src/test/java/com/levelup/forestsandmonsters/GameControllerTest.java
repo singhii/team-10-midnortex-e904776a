f0de1239744505927e860a7a56c2948b575a3fa8
root <rwb304@hotmail.com>
2023-11-17 10:56:45 -0500

    added character test

--- src/test/java/com/levelup/forestsandmonsters/GameControllerTest.java
+++ src/test/java/com/levelup/forestsandmonsters/GameControllerTest.java
@@ -9,5 +9,17 @@ public class GameControllerTest {
         GameController testObj = new GameController();
         assertNotNull(testObj.status);
     }
+
+    @Test
+    public void createCharacterTest() {
+        GameController testobj = new GameController();
+        testobj.createCharacter("Tester");
+            assertNotNull(testobj.status);
+    }
+
     
+
+    
+    
+
 }
